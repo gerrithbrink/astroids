43f2c22789b707a8ca6c38fc73a4fe3258a309dc
gerrit <gerritb@eezi.io>
2026-04-01 00:35:18 +0200

    last commit

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_state, log_event
 from player import Player
 from asteroids import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
+import sys
+import random
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -16,9 +19,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     asteriod_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -29,6 +34,33 @@ def main():
                 return
         screen.fill('black')
         updatable.update(dt)
+        for ast in asteroids:
+            col = player.collides_with(ast)
+            if col:
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                hit = ast.collides_with(shot)
+                if hit:
+                    log_event("asteroid_shot")
+                    ast.kill()
+                    shot.kill()
+                    if ast.radius <= ASTEROID_MIN_RADIUS:
+                        return
+                    else:
+                        log_event("asteroid_split")
+                        deg = random.uniform(20, 50)
+                        new_ast_1 = Asteroid(ast.position.x, ast.position.y, ast.radius - ASTEROID_MIN_RADIUS)
+                        new_ast_2 = Asteroid(ast.position.x, ast.position.y, ast.radius - ASTEROID_MIN_RADIUS)
+                        new_ast_1.velocity = ast.velocity.rotate(deg) * 1.2
+                        new_ast_2.velocity = ast.velocity.rotate(-deg) * 1.2
+
+
+
+
+
+
         for draw1 in drawable:
             draw1.draw(screen)
         pygame.display.flip()
--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
+from shot import Shot
 import pygame
 
 class Player(CircleShape):
     def __init__(self, x, y):
         self.rotation  = 0
+        self.shot_time = 0
         super().__init__(x, y, PLAYER_RADIUS)
+       
         
 
     # in the Player class
@@ -24,6 +27,9 @@ class Player(CircleShape):
         self.rotation = self.rotation + (dt * PLAYER_TURN_SPEED)
         
     def update(self, dt):
+
+        self.shot_time = self.shot_time - dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -35,11 +41,24 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
+        if keys[pygame.K_SPACE]:
+            if self.shot_time > 0:
+                pass
+            else:
+                self.shoot()
+                self.shot_time = PLAYER_SHOOT_COOLDOWN_SECONDS
+                
+
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
 
 
 
